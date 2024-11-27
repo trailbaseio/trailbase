dec75bfbbd9d9838758e759a9d7f1b166620da3f
Marcel <ms290498@gmail.com>
2024-11-27 22:50:24 +0100

    Update SettingsPage.tsx - fixed a small typo (#6)
    * Update SettingsPage.tsx - fixed a small typo
    
    Found & fixed a small typo found while checking out the live demo
    
    * Update SettingsPage.tsx

--- ui/admin/src/components/settings/SettingsPage.tsx
+++ ui/admin/src/components/settings/SettingsPage.tsx
@@ -106,8 +106,8 @@ function ServerSettings(props: CommonProps) {
                   ),
                   info: (
                     <p>
-                      A background ask periodically cleans up logs older than
-                      above retention period. Setting the retention to zero turn
+                      A background task periodically cleans up logs older than
+                      above retention period. Setting the retention to zero turns
                       off the cleanup and logs will be retained indefinitely.
                     </p>
                   ),
