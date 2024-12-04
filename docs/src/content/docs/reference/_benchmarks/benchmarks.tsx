996bc277880257c2d785200f125d3bd888ce273a
Sebastian Jeltsch <sebastian@trailbase.io>
2024-12-04 21:27:07 +0100

    Update performance measurements and benchmark article post SQLite execution model changes.

--- docs/src/content/docs/reference/_benchmarks/benchmarks.tsx
+++ docs/src/content/docs/reference/_benchmarks/benchmarks.tsx
@@ -63,6 +63,7 @@ const durations100k = {
     label: "Payload v3+SQLite",
     data: [656.09],
     backgroundColor: colors.payload,
+    hidden: true,
   },
   supabase: {
     label: "SupaBase",
@@ -79,35 +80,45 @@ const durations100k = {
     data: [62.8136],
   },
   pocketbase_dart_jit: {
-    label: "PocketBase Dart (JIT)",
+    label: "PocketBase Dart",
     data: [61.687],
     backgroundColor: colors.pocketbase1,
   },
   trailbase_ts: {
     label: "TrailBase TS",
-    data: [16.742],
+    data: [16.502],
     backgroundColor: colors.trailbase0,
   },
   trailbase_dart_aot: {
-    label: "TrailBase Dart (AOT)",
-    data: [11.1],
+    // AOT
+    label: "TrailBase Dart",
+    data: [7.0869],
+    backgroundColor: colors.trailbase1,
   },
   trailbase_dart_jit: {
+    // JIT
     label: "TrailBase Dart",
-    data: [9.4247],
+    data: [8.0667],
     backgroundColor: colors.trailbase1,
   },
   // {
   //   label: "TrailBase Dart (JIT + PGO)",
   //   data: [10.05],
   // },
-  trailbase_dart_jit_int_pk: {
-    label: "TrailBase Dart (INT PK)",
-    data: [8.5249],
+  // STALE:
+  // trailbase_dart_jit_int_pk: {
+  //   label: "TrailBase Dart (INT PK)",
+  //   data: [8.5249],
+  //   backgroundColor: colors.trailbase2,
+  // },
+  trailbase_dart_dotnet: {
+    // Inserted 100000 messages, took 00:00:05.7071809 (limit=64) (C#)
+    label: "TrailBase C#",
+    data: [5.7071],
     backgroundColor: colors.trailbase2,
   },
   drizzle: {
-    label: "In-process SQLite (Drizzle)",
+    label: "Drizzel SQLite (Node.js)",
     data: [8.803],
     backgroundColor: colors.drizzle,
   },
@@ -117,43 +128,55 @@ export function Duration100kInsertsChart() {
   const data: ChartData<"bar"> = {
     labels: ["Time in seconds (lower is faster)"],
     datasets: [
-      durations100k.supabase,
-      durations100k.pocketbase_ts,
-      durations100k.pocketbase_dart_jit,
-      durations100k.trailbase_ts,
-      durations100k.trailbase_dart_jit,
-      durations100k.drizzle,
-    ] as ChartDataset<"bar">[],
-  };
-
-  return <BarChart data={data} />;
-}
-
-export function Duration100kInsertsChartMoreResults() {
-  const data: ChartData<"bar"> = {
-    labels: ["Time in seconds (lower is faster)"],
-    datasets: [
       durations100k.payload,
       durations100k.supabase,
       durations100k.pocketbase_ts,
       durations100k.pocketbase_dart_jit,
       durations100k.trailbase_ts,
-      durations100k.trailbase_dart_jit,
+      durations100k.trailbase_dart_aot,
+      durations100k.trailbase_dart_dotnet,
       durations100k.drizzle,
     ] as ChartDataset<"bar">[],
   };
 
-  return <BarChart data={data} />;
+  return (
+    <BarChart
+      data={data}
+      scales={{
+        y: {
+          display: true,
+          // type: "logarithmic",
+        },
+      }}
+    />
+  );
 }
 
 export function PocketBaseAndTrailBaseReadLatencies() {
   // 2024-10-12
-  // TB: Read 1 000 000 messages, took 0:00:57.952120 (limit=64)
+  // TB: Read 1 000 000 messages, took 0:00:57.952120 (limit=64) (Dart JIT)
+  // const readTrailbaseMicroS = {
+  //   p50: 3504,
+  //   p75: 3947,
+  //   p90: 4393,
+  //   p95: 4725,
+  // };
+  // 2024-12-04
+  // TB: Read 1 000 000 messages, took 0:00:55.025486 (limit=64) (Dart AOT)
   const readTrailbaseMicroS = {
-    p50: 3504,
-    p75: 3947,
-    p90: 4393,
-    p95: 4725,
+    p50: 3291,
+    p75: 3590,
+    p90: 4027,
+    p95: 4428,
+  };
+
+  // 2024-12-05
+  // TB: Read 1 000 000 messages, took 00:00:21.8387601 (limit=64) (C#)
+  const readTrailbaseDotnetMicroS = {
+    p50: 947.5,
+    p75: 1240.9,
+    p90: 1553.3,
+    p95: 1895.4,
   };
 
   // 2024-10-12
@@ -179,7 +202,12 @@ export function PocketBaseAndTrailBaseReadLatencies() {
       {
         label: "TrailBase",
         data: latenciesMs(readTrailbaseMicroS),
-        backgroundColor: colors.trailbase0,
+        backgroundColor: colors.trailbase1,
+      },
+      {
+        label: "TrailBase C#",
+        data: latenciesMs(readTrailbaseDotnetMicroS),
+        backgroundColor: colors.trailbase2,
       },
     ],
   };
@@ -201,15 +229,33 @@ export function PocketBaseAndTrailBaseReadLatencies() {
 
 export function PocketBaseAndTrailBaseInsertLatencies() {
   // 2024-10-12
-  // Inserted 10000 messages, took 0:00:01.654810 (limit=64)
+  // TB: Inserted 10 000 messages, took 0:00:01.654810 (limit=64) (Dart JIT)
+  // const insertTrailbaseMicroS = {
+  //   p50: 8107,
+  //   p75: 10897,
+  //   p90: 15327,
+  //   p95: 19627,
+  // };
+  // 2024-12-04
+  // TB: Inserted 10 000 messages, took 0:00:00.863628 (limit=64) (Dart AOT)
   const insertTrailbaseMicroS = {
-    p50: 8107,
-    p75: 10897,
-    p90: 15327,
-    p95: 19627,
+    p50: 5070,
+    p75: 5695,
+    p90: 6615,
+    p95: 7371,
   };
+
+  // 2024-12-05
+  // TB: Inserted 10 000 messages, took 00:00:00.5542653 (limit=64) (C#)
+  const insertTrailbaseDotnetMicroS = {
+    p50: 3348.9,
+    p75: 3810,
+    p90: 4246.5,
+    p95: 4489.9,
+  };
+
   // 2024-10-12
-  //Inserted 10000 messages, took 0:00:07.759677 (limit=64)
+  // PB: Inserted 10 000 messages, took 0:00:07.759677 (limit=64)
   const insertPocketbaseMicroS = {
     p50: 28160,
     p75: 58570,
@@ -231,7 +277,12 @@ export function PocketBaseAndTrailBaseInsertLatencies() {
       {
         label: "TrailBase",
         data: latenciesMs(insertTrailbaseMicroS),
-        backgroundColor: colors.trailbase0,
+        backgroundColor: colors.trailbase1,
+      },
+      {
+        label: "TrailBase C#",
+        data: latenciesMs(insertTrailbaseDotnetMicroS),
+        backgroundColor: colors.trailbase2,
       },
     ],
   };
@@ -346,10 +397,13 @@ export function SupaBaseCpuUsageChart() {
 export function PocketBaseAndTrailBaseUsageChart() {
   // specific run:
   //   PB: Inserted 100000 messages, took 0:01:02.549495 (limit=64)
-  //   TB: Inserted 100000 messages, took 0:00:09.946495 (limit=64)
+  //   TB: Inserted 100000 messages, took 0:00:07.086891 (limit=64) (Dart AOT)
+  //   TB: Inserted 100000 messages, took 00:00:05.7039362 (limit=64) (C#)
 
   const trailbaseUtilization = insertTrailBase as Datum[];
   const pocketbaseUtilization = insertPocketBase as Datum[];
+
+  const trailbaseTimeOffset = -5.1 * 1000;
 
   const data: ChartData<"scatter"> = {
     datasets: [
@@ -357,7 +411,7 @@ export function PocketBaseAndTrailBaseUsageChart() {
         yAxisID: "yLeft",
         label: "TrailBase CPU",
         data: trailbaseUtilization.map((datum) => ({
-          x: datum.elapsed,
+          x: datum.elapsed + trailbaseTimeOffset,
           y: datum.cpu,
         })),
         borderColor: colors.trailbase0,
@@ -368,7 +422,7 @@ export function PocketBaseAndTrailBaseUsageChart() {
         yAxisID: "yRight",
         label: "TrailBase RSS",
         data: trailbaseUtilization.map((datum) => ({
-          x: datum.elapsed,
+          x: datum.elapsed + trailbaseTimeOffset,
           y: datum.rss,
         })),
         borderColor: colors.trailbase1,
@@ -429,6 +483,7 @@ export function PocketBaseAndTrailBaseUsageChart() {
           },
         },
         x: {
+          min: 0,
           ticks: {
             display: true,
             callback: transformMillisecondTicks,
