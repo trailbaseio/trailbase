8b2f535344ffbb4adba55e8b8f9fa306ab3e6a9d
Sebastian Jeltsch <sebastian@trailbase.io>
2024-12-05 22:39:40 +0100

    Update Rust dependencies.

--- ui/admin/tests/util.test.ts
+++ ui/admin/tests/util.test.ts
@@ -2,15 +2,13 @@ import { expect, test } from "vitest"
 import { copyAndConvertRow } from "@/lib/convert";
 
 type UnknownRow = { [key: string]: unknown };
-// eslint-disable-next-line @typescript-eslint/no-wrapper-object-types
-type ObjectRow = { [key: string]: Object | undefined };
 
 test("utils", () => {
   const x: UnknownRow = {
     "foo": "test",
     "bar": "test",
   };
-  const y: ObjectRow = copyAndConvertRow(x);
+  const y = copyAndConvertRow(x);
   for (const key of Object.keys(x)) {
     expect(x[key]).toBe(y[key]);
   }
