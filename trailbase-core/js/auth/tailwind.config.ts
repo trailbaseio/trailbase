98922eedb6438be9f22f708e22e44f69ddfe8fed
Sebastian Jeltsch <sebastian@trailbase.io>
2025-01-05 22:36:27 +0100

    Further reduce reliance on symlinks. We're still relying on symlinks for assets, thus Windows builds will still require a correct symlink setup (git + permissions).

--- trailbase-core/js/auth/tailwind.config.ts
+++ trailbase-core/js/auth/tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
 
-import { commonTailwindConfig } from "./common/tailwind.config.mjs";
+import { commonTailwindConfig } from "../common/tailwind.config.mjs";
 
 export default commonTailwindConfig satisfies Config;
