4be09e8ab5167891efc4688c2b106e1cecedb589
Sebastian Jeltsch <sebastian@trailbase.io>
2025-02-11 15:34:07 +0100

    Reduce logs noise in unit tests.

--- trailbase-core/src/app_state.rs
+++ trailbase-core/src/app_state.rs
@@ -288,6 +288,11 @@ pub async fn test_state(options: Option<TestStateOptions>) -> anyhow::Result<App
   use crate::config::validate_config;
   use crate::migrations::{apply_logs_migrations, apply_main_migrations, apply_user_migrations};
 
+  let _ = env_logger::try_init_from_env(
+    env_logger::Env::new()
+      .default_filter_or("info,refinery_core=warn,tracing::span=warn,swc_ecma_codegen=off"),
+  );
+
   let temp_dir = temp_dir::TempDir::new()?;
   tokio::fs::create_dir_all(temp_dir.child("uploads")).await?;
 
