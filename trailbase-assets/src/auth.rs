7720cc53deeb937da4b0c4ad502ea01f686b60e8
Sebastian Jeltsch <sebastian@trailbase.io>
2025-05-07 17:11:14 +0200

    Fix escaping of hidden form state in auth UI after moving to askama templates a while back. Also added tests.

--- trailbase-assets/src/auth.rs
+++ trailbase-assets/src/auth.rs
@@ -42,3 +42,125 @@ pub struct ChangeEmailTemplate<'a> {
   pub state: String,
   pub alert: &'a str,
 }
+
+pub fn hidden_input<T>(name: &str, value: Option<T>) -> String
+where
+  T: AsRef<str>,
+{
+  if let Some(value) = value {
+    return format!(
+      r#"<input name="{name}" type="hidden" value="{value}" />"#,
+      value = value.as_ref()
+    );
+  }
+  return "".to_string();
+}
+
+pub fn redirect_to<T>(redirect_to: Option<T>) -> String
+where
+  T: AsRef<str>,
+{
+  return hidden_input("redirect_to", redirect_to);
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_login_template_escaping() {
+    let state = hidden_input("TEST", Some("FOO"));
+    let alert = "<><>";
+
+    let template = LoginTemplate {
+      state: state.clone(),
+      alert,
+      enable_registration: true,
+    }
+    .render()
+    .unwrap();
+
+    assert!(template.contains(&state), "{template}"); // Not escaped.
+    assert!(!template.contains(alert), "{template}"); // Is escaped.
+  }
+
+  #[test]
+  fn test_register_template_escaping() {
+    let state = hidden_input("TEST", Some("FOO"));
+    let alert = "<><>";
+
+    let template = RegisterTemplate {
+      state: state.clone(),
+      alert,
+    }
+    .render()
+    .unwrap();
+
+    assert!(template.contains(&state), "{template}"); // Not escaped.
+    assert!(!template.contains(alert), "{template}"); // Is escaped.
+  }
+
+  #[test]
+  fn test_reset_password_request_template_escaping() {
+    let state = hidden_input("TEST", Some("FOO"));
+    let alert = "<><>";
+
+    let template = ResetPasswordRequestTemplate {
+      state: state.clone(),
+      alert,
+    }
+    .render()
+    .unwrap();
+
+    assert!(template.contains(&state), "{template}"); // Not escaped.
+    assert!(!template.contains(alert), "{template}"); // Is escaped.
+  }
+
+  #[test]
+  fn test_reset_password_update_template_escaping() {
+    let state = hidden_input("TEST", Some("FOO"));
+    let alert = "<><>";
+
+    let template = ResetPasswordUpdateTemplate {
+      state: state.clone(),
+      alert,
+    }
+    .render()
+    .unwrap();
+
+    assert!(template.contains(&state), "{template}"); // Not escaped.
+    assert!(!template.contains(alert), "{template}"); // Is escaped.
+  }
+
+  #[test]
+  fn test_change_password_template_escaping() {
+    let state = hidden_input("TEST", Some("FOO"));
+    let alert = "<><>";
+
+    let template = ChangePasswordTemplate {
+      state: state.clone(),
+      alert,
+    }
+    .render()
+    .unwrap();
+
+    assert!(template.contains(&state), "{template}"); // Not escaped.
+    assert!(!template.contains(alert), "{template}"); // Is escaped.
+  }
+
+  #[test]
+  fn test_change_email_template_escaping() {
+    let state = hidden_input("TEST", Some("FOO"));
+    let alert = "<><>";
+
+    let template = ChangeEmailTemplate {
+      state: state.clone(),
+      alert,
+    }
+    .render()
+    .unwrap();
+
+    assert!(template.contains(&state), "{template}"); // Not escaped.
+    assert!(!template.contains(alert), "{template}"); // Is escaped.
+  }
+}
