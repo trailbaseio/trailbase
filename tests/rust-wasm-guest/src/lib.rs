cb9fd12a48e89593ef3e18b2d37eaf1192a44caa
Sebastian Jeltsch <sebastian@trailbase.io>
2025-08-15 22:13:10 +0200

    Wire up http routes from wasm.

--- tests/rust-wasm-guest/src/lib.rs
+++ tests/rust-wasm-guest/src/lib.rs
@@ -1,3 +1,4 @@
+use trailbase_wasm_guest::MethodType;
 use wstd::http::body::{BodyForthcoming, IncomingBody};
 use wstd::http::server::{Finished, Responder};
 use wstd::http::{Request, Response, StatusCode};
@@ -10,7 +11,7 @@ impl trailbase_wasm_guest::Guest for InitEndpoint {
   fn init() -> trailbase_wasm_guest::InitResult {
     println!("init() called");
     return trailbase_wasm_guest::InitResult {
-      http_handlers: vec![],
+      http_handlers: vec![(MethodType::Get, "/wasm".to_string())],
       job_handlers: vec![],
     };
   }
@@ -21,24 +22,19 @@ trailbase_wasm_guest::wit::export!(InitEndpoint);
 // TODO: Ship our own macro when making rust available as a supported guest language.
 #[wstd::http_server]
 async fn main(request: Request<IncomingBody>, responder: Responder) -> Finished {
+  println!("Hello from WASM guest: {}", request.uri().path());
   return match request.uri().path() {
-    "/" => {
-      let msg = std::future::ready("Hello! - from root HTTP handler").await;
-      println!("{msg}");
-
-      let rows = trailbase_wasm_guest::query("SELECT COUNT(*) FROM TEST", vec![])
-        .await
-        .unwrap();
+    // TODO: Build an abstraction to sync init handlers with http handlers here both for http and
+    // jobs..
+    "/wasm" => write_all(responder, format!("Welcome from WASM").as_bytes()).await,
+    "/query" => {
+      let query = std::future::ready("SELECT COUNT(*) FROM TEST").await;
+      let rows = trailbase_wasm_guest::query(&query, vec![]).await.unwrap();
       if rows[0][0] != serde_json::json!(1) {
         panic!("Expected one");
       }
 
-      let mut body = responder.start_response(Response::new(BodyForthcoming));
-      let result = body
-        .write_all(format!("response: {rows:?}").as_bytes())
-        .await;
-
-      Finished::finish(body, result, None)
+      write_all(responder, format!("response: {rows:?}").as_bytes()).await
     }
     _ => {
       let response = Response::builder()
@@ -48,4 +44,10 @@ async fn main(request: Request<IncomingBody>, responder: Responder) -> Finished 
       responder.respond(response).await
     }
   };
+}
+
+async fn write_all(responder: Responder, buf: &[u8]) -> Finished {
+  let mut body = responder.start_response(Response::new(BodyForthcoming));
+  let result = body.write_all(buf).await;
+  Finished::finish(body, result, None)
 }
