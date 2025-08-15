8e2e9fd4e8b57af02e05ed494e7323da98c42c97
Sebastian Jeltsch <sebastian@trailbase.io>
2025-08-15 11:04:22 +0200

    PoC: call sqlite from WASM.

--- tests/rust-wasm-guest/src/lib.rs
+++ tests/rust-wasm-guest/src/lib.rs
@@ -3,56 +3,49 @@ use wstd::http::server::{Finished, Responder};
 use wstd::http::{Request, Response, StatusCode};
 use wstd::io::{AsyncWrite, empty};
 
-wit_bindgen::generate!({
-    world: "trailbase:runtime/trailbase",
-    path: [
-        // Order-sensitive: will import *.wit from the folder.
-        "../../crates/wasm-runtime/wit/deps-0.2.6/random",
-        "../../crates/wasm-runtime/wit/deps-0.2.6/io",
-        "../../crates/wasm-runtime/wit/deps-0.2.6/clocks",
-        "../../crates/wasm-runtime/wit/deps-0.2.6/filesystem",
-        "../../crates/wasm-runtime/wit/deps-0.2.6/sockets",
-        "../../crates/wasm-runtime/wit/deps-0.2.6/cli",
-        "../../crates/wasm-runtime/wit/deps-0.2.6/http",
-        "../../crates/wasm-runtime/wit/trailbase.wit",
-    ],
-    generate_all,
-});
-
 // Implement the function exported in this world (see above).
 struct InitEndpoint;
 
-impl crate::exports::trailbase::runtime::init_endpoint::Guest for InitEndpoint {
-  fn init() -> crate::exports::trailbase::runtime::init_endpoint::InitResult {
+impl trailbase_wasm_guest::Guest for InitEndpoint {
+  fn init() -> trailbase_wasm_guest::InitResult {
     println!("init() called");
-    return crate::exports::trailbase::runtime::init_endpoint::InitResult {
+    return trailbase_wasm_guest::InitResult {
       http_handlers: vec![],
       job_handlers: vec![],
     };
   }
 }
 
-export!(InitEndpoint);
+trailbase_wasm_guest::wit::export!(InitEndpoint);
 
-async fn http_not_found(_request: Request<IncomingBody>, responder: Responder) -> Finished {
-  let response = Response::builder()
-    .status(StatusCode::NOT_FOUND)
-    .body(empty())
-    .unwrap();
-  responder.respond(response).await
-}
-
+// TODO: Ship our own macro when making rust available as a supported guest language.
 #[wstd::http_server]
 async fn main(request: Request<IncomingBody>, responder: Responder) -> Finished {
-  match request.uri().path_and_query().unwrap().as_str() {
+  return match request.uri().path() {
     "/" => {
       let msg = std::future::ready("Hello! - from root HTTP handler").await;
       println!("{msg}");
 
+      let rows = trailbase_wasm_guest::query("SELECT COUNT(*) FROM TEST", vec![])
+        .await
+        .unwrap();
+      if rows[0][0] != serde_json::json!(1) {
+        panic!("Expected one");
+      }
+
       let mut body = responder.start_response(Response::new(BodyForthcoming));
-      let result = body.write_all(b"response").await;
+      let result = body
+        .write_all(format!("response: {rows:?}").as_bytes())
+        .await;
+
       Finished::finish(body, result, None)
     }
-    _ => http_not_found(request, responder).await,
-  }
+    _ => {
+      let response = Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(empty())
+        .unwrap();
+      responder.respond(response).await
+    }
+  };
 }
