581377acc205fd068e717732f003d4377e3a0b82
Sebastian Jeltsch <sebastian@trailbase.io>
2025-08-17 22:26:16 +0200

    PoC: Wire up JS http. Quick benchmark results fib(40): rust-wasm: 14s, js-aot-wasm: 26s, js-wasm: 42s.

--- tests/js-wasm-guest/guest.js
+++ tests/js-wasm-guest/guest.js
@@ -7,24 +7,51 @@ import {
   Fields,
 } from 'wasi:http/types@0.2.3';
 
+// import { MethodType } from 'trailbase:runtime/init-endpoint'
+
+function writeResponse(responseOutparam, status, body) {
+  const outgoingResponse = new OutgoingResponse(new Fields());
+
+  // Access the outgoing response body
+  let outgoingBody = outgoingResponse.body();
+  {
+    // Create a stream for the response body
+    let outputStream = outgoingBody.write();
+    outputStream.blockingWriteAndFlush(
+      new Uint8Array(new TextEncoder().encode(body))
+    );
+    outputStream[Symbol.dispose]();
+  }
+
+  outgoingResponse.setStatusCode(status);
+  OutgoingBody.finish(outgoingBody, undefined);
+  ResponseOutparam.set(responseOutparam, { tag: 'ok', val: outgoingResponse });
+}
+
+function fibonacci(num) {
+  switch (num) {
+    case 0:
+      return 0;
+    case 1:
+      return 1;
+    default:
+      return fibonacci(num - 1) + fibonacci(num - 2);
+  }
+}
+
 export const incomingHandler = {
   handle: async function(incomingRequest, responseOutparam) {
-    const outgoingResponse = new OutgoingResponse(new Fields());
+    const path = incomingRequest.pathWithQuery();
+    console.log(`HTTP request: ${path}`);
 
-    // Access the outgoing response body
-    let outgoingBody = outgoingResponse.body();
-    {
-      // Create a stream for the response body
-      let outputStream = outgoingBody.write();
-      outputStream.blockingWriteAndFlush(
-        new Uint8Array(new TextEncoder().encode('Hello from Javascript!\n'))
-      );
-      outputStream[Symbol.dispose]();
+    switch (path) {
+      case '/fibonacci':
+        writeResponse(responseOutparam, 200, fibonacci(40));
+        break;
+      default:
+        writeResponse(responseOutparam, 200, 'Hello from Javascript!\n');
+        break;
     }
-
-    outgoingResponse.setStatusCode(200);
-    OutgoingBody.finish(outgoingBody, undefined);
-    ResponseOutparam.set(responseOutparam, { tag: 'ok', val: outgoingResponse });
   }
 };
 
@@ -39,7 +66,9 @@ export const incomingHandler = {
 export const initEndpoint = {
   init: async function() {
     return {
-      httpHandlers: [],
+      httpHandlers: [
+        ['get', '/fibonacci'],
+      ],
       jobHandlers: [],
     };
   }
