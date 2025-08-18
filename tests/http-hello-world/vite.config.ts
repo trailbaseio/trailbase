9dfd48ca8914b1fdb3d0a192e7c3bff1752d1f8b
Sebastian Jeltsch <sebastian@trailbase.io>
2025-08-18 17:16:26 +0200

    Break out a separate wasm runtime node package.

--- tests/http-hello-world/vite.config.ts
+++ tests/http-hello-world/vite.config.ts
@@ -1,11 +1,6 @@
 import { defineConfig } from "vite";
 
-import dts from 'vite-plugin-dts'
-
 export default defineConfig({
-  plugins: [
-    dts({ rollupTypes: true }),
-  ],
   build: {
     outDir: "./dist",
     minify: false,
