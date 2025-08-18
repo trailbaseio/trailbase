61386ef324b62ab934cd1d27c681b84da1a31be9
Sebastian Jeltsch <sebastian@trailbase.io>
2025-08-18 11:41:12 +0200

    Add a typescript WASM example using the JCO tool rather than the componentize lib.

---
+++ tests/http-hello-world/src/runtime.ts
@@ -0,0 +1,8 @@
+import { getRandomBytes as _ } from 'wasi:random/random@0.2.3';
+import { getDirectories } from 'wasi:filesystem/preopens@0.2.3';
+
+export function listDirectories(): string[] {
+  return getDirectories().map(([_fd, name]) => {
+    return name;
+  });
+}
