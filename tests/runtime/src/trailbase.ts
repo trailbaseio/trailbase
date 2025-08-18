da942aca7e59c9c8a9b5379b5bb68ea71f05aa39
Sebastian Jeltsch <sebastian@trailbase.io>
2025-08-18 13:19:29 +0200

    Add a placeholder wasm-js runtime.

---
+++ tests/runtime/src/trailbase.ts
@@ -0,0 +1,8 @@
+// export * as wit from "../generated/types/wit";
+
+// export type * as wit from "../generated/types/wit.d.ts";
+// export type * as wit from "@wit/wit";
+
+export function test(): string {
+  return "test";
+}
---
+++ tests/runtime/tests/runtime.test.ts
@@ -0,0 +1,5 @@
+import { test, expect } from "vitest";
+
+test("string handler", async () => {
+  expect(true).toBe(true);
+});
