6344d7eba6e9e6c279bdd28de32a04e1044093d0
Sebastian Jeltsch <sebastian@trailbase.io>
2025-08-20 23:15:01 +0200

    Add sync DB query/execute methods.

--- crates/wasm-runtime-guest/src/db.rs
+++ crates/wasm-runtime-guest/src/db.rs
@@ -12,55 +12,6 @@ pub enum Error {
   UnexpectedType,
 }
 
-// pub enum Value {
-//   Null,
-//   String(String),
-//   Blob(Vec<u8>),
-//   Integer(i64),
-//   Real(f64),
-// }
-//
-// impl TryFrom<serde_json::Value> for Value {
-//   type Error = Error;
-//
-//   fn try_from(value: serde_json::Value) -> Result<Self, Self::Error> {
-//     return match value {
-//       serde_json::Value::Null => Ok(Self::Null),
-//       serde_json::Value::String(s) => Ok(Self::String(s)),
-//       serde_json::Value::Object(mut map) => match map.remove("blob") {
-//         Some(serde_json::Value::String(str)) => Ok(Value::Blob(BASE64_URL_SAFE.decode(&str)?)),
-//         _ => Err(Error::UnexpectedType),
-//       },
-//       serde_json::Value::Number(n) => {
-//         if let Some(n) = n.as_i64() {
-//           Ok(Self::Integer(n))
-//         } else if let Some(n) = n.as_u64() {
-//           Ok(Value::Integer(n as i64))
-//         } else if let Some(n) = n.as_f64() {
-//           Ok(Value::Real(n))
-//         } else {
-//           Err(Error::NotANumber)
-//         }
-//       }
-//       _ => Err(Error::UnexpectedType),
-//     };
-//   }
-// }
-//
-// impl From<Value> for serde_json::Value {
-//   fn from(value: Value) -> Self {
-//     return match value {
-//       Value::Null => Self::Null,
-//       Value::String(s) => Self::String(s),
-//       Value::Integer(i) => Self::Number(serde_json::Number::from(i)),
-//       Value::Real(f) => Self::Number(serde_json::Number::from_f64(f).unwrap()),
-//       Value::Blob(blob) => serde_json::json!({
-//           "blob": BASE64_URL_SAFE.encode(blob)
-//       }),
-//     };
-//   }
-// }
-
 pub struct Transaction {
   committed: bool,
 }
