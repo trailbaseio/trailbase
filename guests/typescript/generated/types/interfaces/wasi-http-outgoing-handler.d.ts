8b5ed1ff2c7639dd8ed6afce28cbf4a01b2839ea
Sebastian Jeltsch <sebastian@trailbase.io>
2025-08-21 21:33:34 +0200

    Minor: move packages around.

--- guests/typescript/generated/types/interfaces/wasi-http-outgoing-handler.d.ts
+++ guests/typescript/generated/types/interfaces/wasi-http-outgoing-handler.d.ts
@@ -3,10 +3,10 @@
  * This function is invoked with an outgoing HTTP Request, and it returns
  * a resource `future-incoming-response` which represents an HTTP Response
  * which may arrive in the future.
- * 
+ *
  * The `options` argument accepts optional parameters for the HTTP
  * protocol's transport layer.
- * 
+ *
  * This function may return an error if the `outgoing-request` is invalid
  * or not allowed to be made. Otherwise, protocol errors are reported
  * through the `future-incoming-response`.
--- guests/typescript/generated/types/interfaces/wasi-io-error.d.ts
+++ guests/typescript/generated/types/interfaces/wasi-io-error.d.ts
@@ -8,7 +8,7 @@ export class Error implements Partial<Disposable> {
   /**
   * Returns a string that is suitable to assist humans in debugging
   * this error.
-  * 
+  *
   * WARNING: The returned string should not be consumed mechanically!
   * It may change across platforms, hosts, or other implementation
   * details. Parsing this string is a major platform-compatibility
