75113f652b9972d1c1971c2626a6bc8f726ea787
Sebastian Jeltsch <sebastian@trailbase.io>
2025-09-04 13:16:45 +0200

    Minor: move http functionality into separate module for JS/TS WASM runtime.

--- guests/typescript/src/index.ts
+++ guests/typescript/src/index.ts
@@ -1,12 +1,145 @@
-export * from "./http";
-export * from "./util";
+import {
+  IncomingRequest,
+  OutgoingResponse,
+  ResponseOutparam,
+} from "wasi:http/types@0.2.3";
+import type { InitResult } from "trailbase:runtime/init-endpoint";
+import type { HttpHandlerInterface, ResponseType } from "./http";
+import {
+  HttpError,
+  Request,
+  StatusCode,
+  encodeBytes,
+  buildResponse,
+} from "./http";
+import type { HttpContext } from "@common/HttpContext";
 
-import { addPeriodicCallback } from "./timer";
+import { addPeriodicCallback, awaitPendingTimers } from "./timer";
 export const timer = {
   addPeriodicCallback,
 };
 
+export * from "./http";
+export * from "./util";
 export { threadId } from "trailbase:runtime/host-endpoint";
 
-import { getRandomBytes as _ } from "wasi:random/random@0.2.3";
-import { getDirectories as __ } from "wasi:filesystem/preopens@0.2.3";
+export type JobHandler = {
+  name: string;
+  spec: string;
+  handler: () => void | Promise<void>;
+};
+
+export interface Config {
+  incomingHandler: {
+    handle: (
+      req: IncomingRequest,
+      respOutparam: ResponseOutparam,
+    ) => Promise<void>;
+  };
+  initEndpoint: {
+    init: () => InitResult;
+  };
+}
+
+export function defineConfig(args: {
+  httpHandlers?: HttpHandlerInterface[];
+  jobHandlers?: JobHandler[];
+}): Config {
+  const init: InitResult = {
+    httpHandlers: (args.httpHandlers ?? []).map((h) => [h.method, h.path]),
+    jobHandlers: (args.jobHandlers ?? []).map((h) => [h.name, h.spec]),
+  };
+
+  const httpHandlers = Object.fromEntries(
+    (args.httpHandlers ?? []).map((h) => [h.path, h.handler]),
+  );
+  const jobHandlers = Object.fromEntries(
+    (args.jobHandlers ?? []).map((h) => [h.name, h.handler]),
+  );
+
+  async function handle(req: IncomingRequest): Promise<ResponseType> {
+    const path: string | undefined = req.pathWithQuery();
+    if (!path) {
+      throw new HttpError(StatusCode.NOT_FOUND, "path not found");
+    }
+
+    const context: HttpContext = JSON.parse(
+      new TextDecoder().decode(req.headers().get("__context")[0]),
+    );
+
+    if (context.kind === "Job") {
+      const handler = jobHandlers[context.registered_path];
+      await handler();
+      return new Uint8Array();
+    } else {
+      const handler = httpHandlers[context.registered_path];
+      if (!handler) {
+        throw new HttpError(StatusCode.NOT_FOUND, "impl not found");
+      }
+
+      return await handler(
+        new Request(
+          req.method(),
+          req.pathWithQuery() ?? "",
+          context.path_params,
+          req.scheme(),
+          req.authority() ?? "",
+          req.headers(),
+          context.user,
+          req.consume(),
+        ),
+      );
+    }
+  }
+
+  return {
+    incomingHandler: {
+      handle: async function (
+        req: IncomingRequest,
+        respOutparam: ResponseOutparam,
+      ) {
+        try {
+          const resp: ResponseType = await handle(req);
+          writeResponse(
+            respOutparam,
+            resp instanceof OutgoingResponse
+              ? resp
+              : buildResponse(
+                  resp instanceof Uint8Array ? resp : encodeBytes(resp),
+                ),
+          );
+        } catch (err) {
+          if (err instanceof HttpError) {
+            writeResponse(
+              respOutparam,
+              buildResponse(encodeBytes(err.message), {
+                status: err.statusCode,
+              }),
+            );
+          } else {
+            writeResponse(
+              respOutparam,
+              buildResponse(encodeBytes(`Caught: ${err}`), {
+                status: StatusCode.INTERNAL_SERVER_ERROR,
+              }),
+            );
+          }
+        } finally {
+          await awaitPendingTimers();
+        }
+      },
+    },
+    initEndpoint: {
+      init: function (): InitResult {
+        return init;
+      },
+    },
+  };
+}
+
+function writeResponse(
+  responseOutparam: ResponseOutparam,
+  response: OutgoingResponse,
+) {
+  ResponseOutparam.set(responseOutparam, { tag: "ok", val: response });
+}
