0db9a90e71cd7967e358672d55ad04fcc62565e9
Sebastian Jeltsch <sebastian@trailbase.io>
2025-09-12 11:47:42 +0200

    Update `examples/collab-clicker-ssr` to tailwind v4.

--- examples/collab-clicker-ssr/vite.config.ts
+++ examples/collab-clicker-ssr/vite.config.ts
@@ -1,21 +1,12 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 import eslint from "vite-plugin-eslint";
-import tailwindcss from "tailwindcss";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    solid({ ssr: true }),
-    // tailwindcss(),
-    eslint(),
-  ],
+  plugins: [solid({ ssr: true }), tailwindcss(), eslint()],
   ssr: {
     noExternal: true,
-  },
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
   },
 });
