7c28bce87ef3104324c66ec089336dfe8423a5b9
Sebastian Jeltsch <sebastian@trailbase.io>
2026-02-17 22:20:13 +0100

    Add GeoJson Geometry JSONSchema support.

--- crates/core/src/schema_metadata.rs
+++ crates/core/src/schema_metadata.rs
@@ -287,6 +287,7 @@ mod tests {
     assert_eq!(
       schema,
       json!({
+        "$schema": "https://json-schema.org/draft/2020-12/schema",
         "title": table_name.name,
         "type": "object",
         "properties": {
