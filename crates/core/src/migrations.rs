a2bd5f52c2e9d4e8db39ca61f7498912473bac5c
Sebastian Jeltsch <sebastian@trailbase.io>
2026-03-02 17:40:12 +0100

    Add migration to remove now unused `_user` columns.

--- crates/core/src/migrations.rs
+++ crates/core/src/migrations.rs
@@ -294,7 +294,7 @@ mod tests {
           (),
           |row| row.get(0),
         )
-        .unwrap();
+        .unwrap_or_default();
     }
 
     fn index_exists(conn: &rusqlite::Connection, name: &str) -> bool {
