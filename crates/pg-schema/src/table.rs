2fa0620886b70722953d2d75428c53d6a6923829
Sebastian Jeltsch <sebastian@trailbase.io>
2026-05-18 16:31:07 +0200

    Add support for PG indexes and triggers.

--- crates/pg-schema/src/table.rs
+++ crates/pg-schema/src/table.rs
@@ -308,6 +308,7 @@ mod tests {
   use trailbase_sqlite::Connection;
 
   use super::*;
+  use crate::util::test_connection;
 
   async fn get_tables_async(conn: &Connection) -> Result<Vec<TableInformationSchema>, Error> {
     return Ok(
@@ -337,34 +338,6 @@ mod tests {
             Err(sql_err) => sql_err.into(),
           };
         })?,
-    );
-  }
-
-  async fn test_connection() -> (pglite_oxide::PgliteServer, Connection) {
-    let temp_dir = tempfile::TempDir::new().unwrap();
-
-    // NOTE: `db.connection_uri()` returns rubbish for UDS.
-    let sock = temp_dir.path().join(".s.PGSQL.5432");
-
-    let db = pglite_oxide::PgliteServer::builder()
-      .fresh_temporary()
-      // .temporary()
-      .unix(&sock)
-      .start()
-      .unwrap();
-
-    let pg_uri = format!(
-      "postgresql://postgres@/template1?host={}",
-      temp_dir.path().to_string_lossy()
-    );
-
-    return (
-      db,
-      trailbase_sqlite::Connection::pg_with_opts(trailbase_sqlite::generic::PgOptions {
-        connection: trailbase_sqlite::generic::PgConnection::Uri(pg_uri),
-        num_threads: Some(1),
-      })
-      .unwrap(),
     );
   }
 
