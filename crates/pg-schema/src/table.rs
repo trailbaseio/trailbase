aad04fb625959193b77f850db0bff8f9d8a12b40
Sebastian Jeltsch <sebastian@trailbase.io>
2026-05-26 09:58:28 +0200

    Fix constraint extraction for non PK columns and extract `CHECK` clauses.

--- crates/pg-schema/src/table.rs
+++ crates/pg-schema/src/table.rs
@@ -84,6 +84,8 @@ pub(crate) struct ColumnInformationSchema {
   // For VIEWs:
   pub source_table: Option<String>,
   pub source_column: Option<String>,
+  // For Check:
+  pub check_clause: Option<String>,
 }
 
 const QUERY_COLUMNS_WITH_CONSTRAINTS: &str = "
@@ -102,25 +104,48 @@ SELECT
     MAX(CASE WHEN tc.constraint_type = 'UNIQUE' THEN tc.constraint_name END) AS unique_constraint,
     MAX(CASE WHEN tc.constraint_type = 'CHECK' THEN tc.constraint_name END) AS check_constraint,
     -- Only for FKs
-    MAX(ccu.table_name) AS fk_table_name,
-    MAX(ccu.column_name) AS fk_column_name,
+    MAX(CASE WHEN tc.constraint_type = 'FOREIGN KEY' THEN fk_ccu.table_name END) AS fk_table_name,
+    MAX(CASE WHEN tc.constraint_type = 'FOREIGN KEY' THEN fk_ccu.column_name END) AS fk_column_name,
     -- Only defined for VIEW columns:
     vcu.table_name AS source_table,
-    vcu.column_name AS source_column
+    vcu.column_name AS source_column,
+    -- Only Checks
+    MAX(chc.check_clause) AS check_clause
 FROM
     information_schema.columns c
+    LEFT JOIN information_schema.constraint_column_usage ccu ON
+        c.table_catalog = ccu.table_catalog
+        AND c.table_schema = ccu.table_schema
+        AND c.table_name = ccu.table_name
+        AND c.column_name = ccu.column_name
+    -- We need this atop `ccu` above to look up FKs
     LEFT JOIN information_schema.key_column_usage kcu ON
-        c.table_schema = kcu.table_schema
+        c.table_catalog = kcu.table_catalog
+        AND c.table_schema = kcu.table_schema
         AND c.table_name = kcu.table_name
         AND c.column_name = kcu.column_name
     LEFT JOIN information_schema.table_constraints tc ON
-        kcu.constraint_name = tc.constraint_name
-        AND kcu.table_schema = tc.table_schema
-        AND kcu.table_name = tc.table_name
-    LEFT JOIN information_schema.constraint_column_usage ccu ON
+        tc.constraint_catalog IN (ccu.constraint_catalog, kcu.constraint_catalog)
+        AND tc.constraint_schema IN (ccu.constraint_schema, kcu.constraint_schema)
+        AND (
+          (
+            ccu.constraint_name = tc.constraint_name
+            -- Avoid back-edges.
+            AND ccu.table_name = tc.table_name
+          ) OR (
+            -- Use `kcu` only for FK lookups.
+            kcu.constraint_name = tc.constraint_name AND tc.constraint_type = 'FOREIGN KEY'
+          )
+        )
+    LEFT JOIN information_schema.constraint_column_usage fk_ccu ON
         tc.constraint_type = 'FOREIGN KEY'
-        AND kcu.constraint_name = ccu.constraint_name
-        AND kcu.table_schema = ccu.table_schema
+        AND tc.constraint_catalog = fk_ccu.constraint_catalog
+        AND tc.constraint_schema = fk_ccu.constraint_schema
+        AND tc.constraint_name = fk_ccu.constraint_name
+    LEFT JOIN information_schema.check_constraints chc ON
+        ccu.constraint_catalog = tc.constraint_catalog
+        AND ccu.constraint_schema = tc.constraint_schema
+        AND ccu.constraint_name = chc.constraint_name
     LEFT JOIN information_schema.view_column_usage vcu ON
         c.table_schema = vcu.view_schema
         AND c.table_name = vcu.view_name
@@ -141,8 +166,6 @@ GROUP BY
     vcu.table_name,
     vcu.column_name
 ORDER BY
-    -- c.table_schema,
-    -- c.table_name,
     c.ordinal_position;
 ";
 
@@ -175,6 +198,7 @@ pub(crate) fn get_columns(
         fk_column_name: row.get(14)?,
         source_table: row.get(15)?,
         source_column: row.get(16)?,
+        check_clause: row.get(17)?,
       });
     })
     .collect::<Result<_, Error>>();
@@ -242,8 +266,8 @@ pub(crate) fn build_column_schema(c: ColumnInformationSchema) -> Result<Column, 
     })
   }
 
-  if let Some(check) = c.check_constraint {
-    options.push(ColumnOption::Check(check));
+  if let Some(clause) = c.check_clause {
+    options.push(ColumnOption::Check(clause));
   }
 
   return Ok(Column {
@@ -346,13 +370,16 @@ mod tests {
     conn
       .execute_batch(
         "
-        CREATE TABLE table0 (id INTEGER PRIMARY KEY NOT NULL);
+        CREATE TABLE table0 (
+          id     INTEGER PRIMARY KEY NOT NULL CHECK(id >= 0)
+        );
 
         CREATE TABLE table1 (
           id     INTEGER PRIMARY KEY,
-          fk     INTEGER REFERENCES table0(id),
-          a      TEXT DEFAULT ('foo'),
-          b      INT8 NOT NULL DEFAULT (5)
+          fk0    INTEGER REFERENCES table0(id),
+          fk1    INTEGER NOT NULL REFERENCES table0,
+          a      TEXT UNIQUE DEFAULT('foo') CHECK(a != ''),
+          b      INT8 NOT NULL DEFAULT(5)
         );
       ",
       )
@@ -368,8 +395,8 @@ mod tests {
     assert!(columns0.len() > 0);
 
     assert_eq!(
-      columns0,
-      vec![ColumnInformationSchema {
+      columns0[0],
+      ColumnInformationSchema {
         table_catalog: "template1".to_string(),
         table_schema: "public".to_string(),
         table_name: "table0".to_string(),
@@ -382,9 +409,10 @@ mod tests {
         primary_key: Some("table0_pkey".to_string()),
         foreign_key: None,
         unique_constraint: None,
-        check_constraint: None,
+        check_constraint: Some("table0_id_check".to_string()),
+        check_clause: Some("(id >= 0)".to_string()),
         ..Default::default()
-      },]
+      }
     );
 
     let columns1 = get_columns_async(&conn, "table1").await.unwrap();
@@ -416,14 +444,14 @@ mod tests {
         table_catalog: "template1".to_string(),
         table_schema: "public".to_string(),
         table_name: "table1".to_string(),
-        column_name: "fk".to_string(),
+        column_name: "fk0".to_string(),
         ordinal_position: 2,
         is_nullable: "YES".to_string(),
         data_type: "integer".to_string(),
         column_default: None,
         is_generated: "NEVER".to_string(),
         primary_key: None,
-        foreign_key: Some("table1_fk_fkey".to_string()),
+        foreign_key: Some("table1_fk0_fkey".to_string()),
         unique_constraint: None,
         check_constraint: None,
         fk_table_name: Some("table0".to_string()),
@@ -438,12 +466,18 @@ mod tests {
         table_catalog: "template1".to_string(),
         table_schema: "public".to_string(),
         table_name: "table1".to_string(),
-        column_name: "a".to_string(),
+        column_name: "fk1".to_string(),
         ordinal_position: 3,
-        is_nullable: "YES".to_string(),
-        data_type: "text".to_string(),
-        column_default: Some("'foo'::text".to_string()),
+        is_nullable: "NO".to_string(),
+        data_type: "integer".to_string(),
+        column_default: None,
         is_generated: "NEVER".to_string(),
+        primary_key: None,
+        foreign_key: Some("table1_fk1_fkey".to_string()),
+        unique_constraint: None,
+        check_constraint: None,
+        fk_table_name: Some("table0".to_string()),
+        fk_column_name: Some("id".to_string()),
         ..Default::default()
       },
     );
@@ -454,8 +488,27 @@ mod tests {
         table_catalog: "template1".to_string(),
         table_schema: "public".to_string(),
         table_name: "table1".to_string(),
-        column_name: "b".to_string(),
+        column_name: "a".to_string(),
         ordinal_position: 4,
+        is_nullable: "YES".to_string(),
+        data_type: "text".to_string(),
+        column_default: Some("'foo'::text".to_string()),
+        is_generated: "NEVER".to_string(),
+        unique_constraint: Some("table1_a_key".to_string()),
+        check_constraint: Some("table1_a_check".to_string()),
+        check_clause: Some("(a <> ''::text)".to_string()),
+        ..Default::default()
+      },
+    );
+
+    assert_eq!(
+      columns1[4],
+      ColumnInformationSchema {
+        table_catalog: "template1".to_string(),
+        table_schema: "public".to_string(),
+        table_name: "table1".to_string(),
+        column_name: "b".to_string(),
+        ordinal_position: 5,
         is_nullable: "NO".to_string(),
         data_type: "bigint".to_string(),
         column_default: Some("5".to_string()),
@@ -515,5 +568,80 @@ mod tests {
       .unwrap();
 
     assert_eq!(1, table2.columns.len());
+  }
+
+  #[tokio::test]
+  async fn postgres_check_jsonschema_test() {
+    let (_db, conn) = test_connection().await;
+
+    conn
+      .execute_batch(
+        "
+        CREATE FUNCTION jsonschema(n TEXT, contents JSONB) RETURNS BOOL AS $$
+          BEGIN
+            CASE n
+              -- We're lying a little here:
+              WHEN 'std.FileUpload' THEN
+                RETURN TRUE;
+              WHEN 'std.FileUploads' THEN
+                RETURN TRUE;
+              ELSE
+                RAISE EXCEPTION 'Not supported alongside PG';
+            END CASE;
+          END;
+        $$ LANGUAGE plpgsql;
+        ",
+      )
+      .await
+      .unwrap();
+
+    conn
+      .execute_batch(
+        "
+        CREATE TABLE table0 (
+          \"id\"       SERIAL PRIMARY KEY CHECK(id >= 0),
+          \"file\"     JSONB CHECK(jsonschema('std.FileUpload', file)),
+          \"files\"    JSONB CHECK(jsonschema('std.FileUploads', files))
+        );
+        ",
+      )
+      .await
+      .unwrap();
+
+    let table_schemas: Vec<Table> = conn
+      .call_writer(|mut conn| {
+        return build_all_table_schemas(&mut conn)
+          .map_err(|err| trailbase_sqlite::Error::Other(err.into()));
+      })
+      .await
+      .unwrap();
+
+    assert_eq!(1, table_schemas.len());
+    let table = &table_schemas[0];
+    assert_eq!("table0", table.name.name);
+
+    let file_col = &table.columns[1];
+    let file_check = file_col
+      .options
+      .iter()
+      .filter_map(|o| match o {
+        ColumnOption::Check(exp) => Some(exp),
+        _ => None,
+      })
+      .collect::<Vec<_>>()[0];
+
+    assert_eq!("jsonschema('std.FileUpload'::text, file)", file_check);
+
+    let files_col = &table.columns[2];
+    let files_check = files_col
+      .options
+      .iter()
+      .filter_map(|o| match o {
+        ColumnOption::Check(exp) => Some(exp),
+        _ => None,
+      })
+      .collect::<Vec<_>>()[0];
+
+    assert_eq!("jsonschema('std.FileUploads'::text, files)", files_check);
   }
 }
