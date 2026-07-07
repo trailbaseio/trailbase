7e50c9ec4e84709c7a4440ad6cec0eda88ba2599
Sebastian Jeltsch <sebastian@trailbase.io>
2026-07-07 19:45:01 +0200

    Add manifest init to JS WASM guest.

--- guests/typescript/src/index.ts
+++ guests/typescript/src/index.ts
@@ -1,20 +1,23 @@
 import { IncomingRequest, ResponseOutparam } from "wasi:http/types@0.2.3";
 import type {
-  Arguments,
-  HttpHandlers,
-  JobHandlers,
-  SqliteFunctions,
-} from "trailbase:component/init-endpoint@0.1.1";
+  // Arguments,
+  // HttpHandlers,
+  // JobHandlers,
+  // SqliteFunctions,
+} from "trailbase:component/init-endpoint@0.2.0";
 import type {
   Arguments as SqliteArguments,
   Error as SqliteError,
   dispatchScalarFunction,
-} from "trailbase:component/sqlite-function-endpoint@0.1.1";
+} from "trailbase:component/sqlite-function-endpoint@0.2.0";
 import type { HttpHandlerInterface } from "./http";
 import type { JobHandlerInterface } from "./job";
 import { buildIncomingHttpHandler } from "./http/incoming";
 
 export { addPeriodicCallback } from "./timer";
+
+import type { InitArguments } from "@bindings/InitArguments";
+import type { InitManifest } from "@bindings/InitManifest";
 
 export * from "./util";
 
@@ -26,9 +29,10 @@ export interface Config {
     ) => Promise<void>;
   };
   initEndpoint: {
-    initHttpHandlers: (args: Arguments) => HttpHandlers;
-    initJobHandlers: (args: Arguments) => JobHandlers;
-    initSqliteFunctions: (args: Arguments) => SqliteFunctions;
+    // initHttpHandlers: (args: Arguments) => HttpHandlers;
+    // initJobHandlers: (args: Arguments) => JobHandlers;
+    // initSqliteFunctions: (args: Arguments) => SqliteFunctions;
+    getManifest: (args: string) => string;
   };
   sqliteFunctionEndpoint: {
     dispatchScalarFunction: typeof dispatchScalarFunction;
@@ -49,36 +53,51 @@ export function defineConfig(opts: {
       handle: buildIncomingHttpHandler(opts),
     },
     initEndpoint: {
-      initHttpHandlers: function (args: Arguments): HttpHandlers {
+      // initHttpHandlers: function(args: Arguments): HttpHandlers {
+      //   opts.init?.({
+      //     version: args.version,
+      //   });
+      //
+      //   return {
+      //     handlers: (opts.httpHandlers ?? []).map((h) => [h.method, h.path]),
+      //   };
+      // },
+      // initJobHandlers: function(args: Arguments): JobHandlers {
+      //   opts.init?.({
+      //     version: args.version,
+      //   });
+      //
+      //   return {
+      //     handlers: (opts.jobHandlers ?? []).map((h) => [h.name, h.spec]),
+      //   };
+      // },
+      // initSqliteFunctions: function(args: Arguments): SqliteFunctions {
+      //   opts.init?.({
+      //     version: args.version,
+      //   });
+      //
+      //   return {
+      //     scalarFunctions: [],
+      //   };
+      // },
+      getManifest: function(jsonArgs: string): string {
+        const args: InitArguments = JSON.parse(jsonArgs);
+
         opts.init?.({
-          version: args.version,
+          version: args.version ?? undefined,
         });
 
-        return {
-          handlers: (opts.httpHandlers ?? []).map((h) => [h.method, h.path]),
+        const manifest: InitManifest = {
+          http_handlers: opts.httpHandlers?.map((h) => ({ method: h.method, path: h.path })) ?? null,
+          job_handlers: opts.jobHandlers?.map((h) => ({ name: h.name, spec: h.spec })) ?? null,
+          sqlite_functions: null,
         };
-      },
-      initJobHandlers: function (args: Arguments): JobHandlers {
-        opts.init?.({
-          version: args.version,
-        });
 
-        return {
-          handlers: (opts.jobHandlers ?? []).map((h) => [h.name, h.spec]),
-        };
-      },
-      initSqliteFunctions: function (args: Arguments): SqliteFunctions {
-        opts.init?.({
-          version: args.version,
-        });
-
-        return {
-          scalarFunctions: [],
-        };
+        return JSON.stringify(manifest);
       },
     },
     sqliteFunctionEndpoint: {
-      dispatchScalarFunction: function (_args: SqliteArguments) {
+      dispatchScalarFunction: function(_args: SqliteArguments) {
         throw {
           tag: "other",
           val: "missing sqlite function",
