2fe6c966fcccb6b188ebcde7692903cb71dc8373
Sebastian Jeltsch <sebastian@trailbase.io>
2026-07-23 13:05:37 +0200

    Fish tokens from local storage :/.

--- crates/auth-ui/dash/src/App.tsx
+++ crates/auth-ui/dash/src/App.tsx
@@ -1,5 +1,6 @@
 // import { createSignal } from "solid-js";
-import { initClientFromCookies, Client } from "trailbase";
+import { initClientFromCookies, Client, initClient } from "trailbase";
+import type { Tokens } from "trailbase";
 
 function App() {
   return (
@@ -16,7 +17,19 @@ function App() {
 }
 
 async function test() {
-  const client: Client = await initClientFromCookies();
+  console.debug(import.meta.env.DEV, document.cookie, localStorage);
+  const authTokens: string | null = localStorage.getItem("auth_tokens");
+
+  let client: Client | UnderlyingDefaultSource;
+  if (authTokens !== null) {
+    const tokens: Tokens = JSON.parse(authTokens);
+
+    client = initClient(document.head.baseURI, {
+      tokens,
+    });
+  } else {
+    client = await initClientFromCookies();
+  }
 
   console.log("TOKENS: ", client.tokens());
 }
