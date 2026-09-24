f9b8eeaa07bd50773d0e38e49e2994c9322554d0
Sebastian Jeltsch <sebastian@trailbase.io>
2026-09-24 16:55:33 +0200

    WIP

--- crates/sqlite/src/generic.rs
+++ crates/sqlite/src/generic.rs
@@ -262,7 +262,7 @@ impl Connection {
     return match self.exec {
       Executor::Sqlite(ref exec) => exec.read_query_rows_f(sql, params, sqlite_from_rows).await,
       Executor::Pg(ref exec) => exec.query_rows_f(sql, params, pg_from_rows).await,
-      Executor::Stoolap(ref _exec) => Err(Error::NotImplemented),
+      Executor::Stoolap(ref exec) => exec.db.clone().query_rows(sql, params),
     };
   }
 
@@ -282,7 +282,7 @@ impl Connection {
           .await
       }
       Executor::Pg(_) => self.write_query_row(sql, params).await,
-      Executor::Stoolap(ref _exec) => Err(Error::NotImplemented),
+      Executor::Stoolap(ref exec) => exec.db.clone().query_row(sql, params),
     };
   }
 
@@ -306,7 +306,18 @@ impl Connection {
           .await
       }
       Executor::Pg(_) => self.write_query_row_get(sql, params, index).await,
-      Executor::Stoolap(ref _exec) => Err(Error::NotImplemented),
+      Executor::Stoolap(ref exec) => {
+        // use stoolap::FromRow;
+        // let rows = exec
+        //   .db
+        //   .query(sql.as_ref(), crate::stoolap::value::map_params(params)?)?;
+        //
+        // if let Some(row) = rows.next() {
+        //   return T::from_row(row?);
+        // }
+
+        Err(Error::NotImplemented)
+      }
     };
   }
 
@@ -318,7 +329,7 @@ impl Connection {
     return match self.exec {
       Executor::Sqlite(ref exec) => exec.write_query_rows_f(sql, params, sqlite_from_rows).await,
       Executor::Pg(ref exec) => exec.query_rows_f(sql, params, pg_from_rows).await,
-      Executor::Stoolap(ref _exec) => Err(Error::NotImplemented),
+      Executor::Stoolap(ref exec) => exec.db.clone().query_rows(sql, params),
     };
   }
 
@@ -346,7 +357,7 @@ impl Connection {
           })
           .await
       }
-      Executor::Stoolap(ref _exec) => Err(Error::NotImplemented),
+      Executor::Stoolap(ref exec) => exec.db.clone().query_row(sql, params),
     };
   }
 
@@ -781,9 +792,16 @@ mod tests {
     let conn = Connection::new(Executor::Stoolap(exec));
 
     let _ = conn
-      .execute("CREATE TABLE test (id INTEGER)", ())
+      .execute_batch("CREATE TABLE test (id INTEGER); INSERT INTO test (id) VALUES (1), (2);")
       .await
       .unwrap();
+
+    let row = conn
+      .read_query_row("SELECT COUNT(*) FROM test;", ())
+      .await
+      .unwrap()
+      .unwrap();
+    assert_eq!(2, row.get::<i64>(0).unwrap())
   }
 
   #[tokio::test]
--- crates/sqlite/src/stoolap/value.rs
+++ crates/sqlite/src/stoolap/value.rs
@@ -1,6 +1,20 @@
-use crate::Error;
-use crate::value::Value;
+use std::iter::zip;
 use stoolap::ToParam;
+
+use crate::Error;
+use crate::rows::{ColVec, Column, Rc, Row, Rows, ValueType};
+use crate::value::Value;
+
+// impl<T: stoolap::FromValue> crate::from_sql::FromSql for T {
+//   fn column_result(value: crate::ValueRef<'_>) -> crate::from_sql::FromSqlResult<Self> {}
+// }
+
+// impl<T: crate::from_sql::FromSql> stoolap::FromRow for T {
+//   fn from_row(row: &stoolap::ResultRow) -> stoolap::Result<T> {
+//       T.
+//       row
+//   }
+// }
 
 impl stoolap::ToParam for Value {
   fn to_param(&self) -> stoolap::Value {
@@ -52,4 +66,88 @@ pub fn map_params(params: impl crate::params::Params) -> Result<Params, Error> {
   let mut p = Params::new();
   params.bind(&mut p)?;
   return Ok(p);
+}
+
+pub fn from_rows(mut rows: stoolap::Rows) -> Result<Rows, Error> {
+  let Some(Ok(first_row)) = rows.next() else {
+    return Ok(Rows::default());
+  };
+
+  let columns = Rc::new(columns(&first_row));
+  let mut result = vec![self::from_row(first_row, columns.clone())?];
+  while let Some(row) = rows.next() {
+    result.push(self::from_row(row?, columns.clone())?);
+  }
+
+  return Ok(Rows {
+    rows: result,
+    columns,
+  });
+}
+
+pub fn from_row(row: stoolap::ResultRow, cols: Rc<ColVec<Column>>) -> Result<Row, Error> {
+  use stoolap::Value as SValue;
+  let values = row
+    .into_inner()
+    .into_iter()
+    .map(|v| {
+      return match v {
+        SValue::Null(_) => Value::Null,
+        SValue::Boolean(b) => Value::Integer(if b { 1 } else { 0 }),
+        SValue::Integer(i) => Value::Integer(i),
+        SValue::Float(f) => Value::Real(f),
+        SValue::Text(s) => Value::Text(s.to_string()),
+        SValue::Timestamp(ts) => Value::Integer(ts.timestamp_millis()),
+        SValue::Extension(b) => Value::Blob(b.to_vec()),
+      };
+    })
+    .collect();
+
+  return Ok(Row {
+    values,
+    columns: cols,
+  });
+}
+
+#[inline]
+pub(crate) fn columns(row: &stoolap::ResultRow) -> ColVec<Column> {
+  use stoolap::Value;
+
+  return row
+    .columns()
+    .iter()
+    .enumerate()
+    .map(|(idx, name)| {
+      return match row.get_value(idx) {
+        None | Some(Value::Null(_)) => Column {
+          name: name.into(),
+          decl_type: ValueType::Null,
+        },
+        Some(Value::Integer(_)) => Column {
+          name: name.into(),
+          decl_type: ValueType::Integer,
+        },
+        Some(Value::Boolean(_)) => Column {
+          name: name.into(),
+          decl_type: ValueType::Integer,
+        },
+        Some(Value::Timestamp(_)) => Column {
+          name: name.into(),
+          decl_type: ValueType::Integer,
+        },
+        Some(Value::Float(_)) => Column {
+          name: name.into(),
+          decl_type: ValueType::Real,
+        },
+        Some(Value::Text(_)) => Column {
+          name: name.into(),
+          decl_type: ValueType::Text,
+        },
+        Some(Value::Extension(_)) => Column {
+          name: name.into(),
+          decl_type: ValueType::Blob,
+        },
+      };
+    })
+    .collect();
 }
